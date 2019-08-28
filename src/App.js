5158f3fce8a4d915df929c342ea24c3a7f0b21af
felgomoli1994 <felipe.gomes@livup.com.br>
2019-08-27 21:31:58 -0300

    envio teste

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ const App = () => {
   const { state, increment, decrement } = Count.useStore()
 
   return (
-    <>
+    <React.Fragment>
       <h1>{state.count}</h1>
 
       <Button onClick={increment}>+</Button>
       <Button onClick={decrement}>-</Button>
-    </>
+    </React.Fragment>
   )
 }
 
